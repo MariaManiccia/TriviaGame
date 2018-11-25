766a623138ae55bef44c8aae77ca737b4a90152a
Maniccia <mariamaniccia@gmail.com>
2018-11-25 15:51:58 -0500

    some html/css/js

---
+++ javascript/app.js
@@ -0,0 +1,31 @@
+// Set the game up to start
+
+// Once the start button is pushed
+
+// a question appears
+
+// and options for answers
+
+//timer starts counting down
+
+// once an answer is choosen
+
+// display correct theme if correct
+
+// disply wrong theme if wrong
+
+// or the timer hits 0
+
+// display the time's out theme
+
+// then go to next question/answer set
+
+// restart timer
+
+// continue until no more questions are left
+
+// display the correct/wrong/time's up answer totals
+
+// display start again button
+
+// if the button is clicked then restart the game.. not the page
