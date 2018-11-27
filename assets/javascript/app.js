35e3fd543f9bd928c49b2692010bfd3027372fb9
Maniccia <mariamaniccia@gmail.com>
2018-11-26 22:11:52 -0500

    next btn

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,8 +30,13 @@ $(document).ready(function () {
         $("#answers3").text(answers.questOne[2]);
         $("#answers4").text(answers.questOne[3]);
 
+        timer();
+
+    });
+
+    function timer() {
         //timer starts counting down
-        var timer2 = "2:00";
+        var timer2 = "1:00";
         var interval = setInterval(function () {
             var timer = timer2.split(':');
             var minutes = parseInt(timer[0], 10);
@@ -42,36 +47,60 @@ $(document).ready(function () {
             seconds = (seconds < 10) ? '0' + seconds : seconds;
             $("#countdown").html(minutes + ':' + seconds);
             if (minutes < 0) clearInterval(interval);
-        // if the counter hits 0...    
+            // if the counter hits 0...    
             timer2 = minutes + ':' + seconds;
-            if ((seconds <= 0) && (minutes <= 0)){
-            clearInterval(interval);
-            $("#countdown").html("You ran out of time!");
+            if ((seconds <= 0) && (minutes <= 0)) {
+                clearInterval(interval);
+                // display the time's out theme
+                $("#countdown").css("visibility", "hidden");
                 $("#answers").css("visibility", "hidden");
                 $("#messageArea").css("visibility", "visible");
-        }
+                $("#time").css("visibility", "hidden");
+                $("#question").css("visibility", "hidden");
+                $("p").html("You ran out of time!");
+            }
         }, 1000);
+    }
 
-        // once an answer is choosen
-
-        // display correct theme if correct
-
-        // disply wrong theme if wrong
-
-        // or the timer hits 0
-
-        // display the time's out theme
-
-        // then go to next question/answer set
-
-        // restart timer
-
-        // continue until no more questions are left
-
-        // display the correct/wrong/time's up answer totals
-
-        // display start again button
-
-        // if the button is clicked then restart the game.. not the page
+    // reset the question screen
+    function resetQ() {
+        $("#countdown").css("visibility", "visible");
+        $("#answers").css("visibility", "visible");
+        $("#messageArea").css("visibility", "hidden");
+        $("#time").css("visibility", "visible");
+        $("#question").css("visibility", "visible");
+    }
+    // if next button is clicked
+    $("#nextQuest").click(function () {
+        // reset page
+        resetQ();
+        // a question appears
+        $("#question").text(questions[1]);
+        // and options for answers
+        $("#answers1").text(answers.questTwo[0]);
+        $("#answers2").text(answers.questTwo[1]);
+        $("#answers3").text(answers.questTwo[2]);
+        $("#answers4").text(answers.questTwo[3]);
+        // start timer
+        timer();
     });
+
+    // if an answer is choosen
+
+    // display correct theme if correct
+
+    // disply wrong theme if wrong
+
+    // if next button is hit
+
+    // restart timer
+
+    // continue until no more questions are left
+
+    // display the correct/wrong/time's up answer totals
+
+    // display start again button
+
+    // if the button is clicked then restart the game.. not the page
+
 });
