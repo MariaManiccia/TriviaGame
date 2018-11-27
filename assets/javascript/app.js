d623cc15368bf9dc3b952660c9df57e2393120de
Maniccia <mariamaniccia@gmail.com>
2018-11-27 00:36:44 -0500

    werk

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,14 +36,14 @@ $(document).ready(function () {
 
     function timer() {
         //timer starts counting down
-        var timer2 = "1:00";
+        var timer2 = "0:30";
         var interval = setInterval(function () {
             var timer = timer2.split(':');
             var minutes = parseInt(timer[0], 10);
             var seconds = parseInt(timer[1], 10);
             --seconds;
             minutes = (seconds < 0) ? --minutes : minutes;
-            seconds = (seconds < 0) ? 59 : seconds;
+            seconds = (seconds < 0) ? 30 : seconds;
             seconds = (seconds < 10) ? '0' + seconds : seconds;
             $("#countdown").html(minutes + ':' + seconds);
             if (minutes < 0) clearInterval(interval);
