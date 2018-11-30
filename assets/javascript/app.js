f4571858dc97e9f4407da0c7eb6e07c21753547b
Maniccia <mariamaniccia@gmail.com>
2018-11-30 14:17:12 -0500

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -204,7 +204,7 @@ $(document).ready(function () {
 
             //starting question/answer combo
             questCount = 0;
-
+            ansCount = 0;
             // a question/answer team appears
             $("#question").html(questAns[questCount].question);
             console.log(questAns[questCount].question);
